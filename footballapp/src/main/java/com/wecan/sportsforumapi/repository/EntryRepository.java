2921d4d41461cc29581540868d1adbae4b000296
ademsoydan <ademm.soydan@gmail.com>
2023-05-27 22:22:34 +0300

    Delete old version

--- footballapp/src/main/java/com/wecan/sportsforumapi/repository/EntryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wecan.sportsforumapi.repository;
-
-import com.wecan.sportsforumapi.entity.Entry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EntryRepository extends JpaRepository<Entry,Integer> {
-}
--- footballapp/src/main/java/com/wecan/sportsforumapi/repository/TitleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wecan.sportsforumapi.repository;
-
-import com.wecan.sportsforumapi.entity.Title;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TitleRepository extends JpaRepository<Title,Integer> {
-}

130507625587ec7f2831e0feefd3559ebd8862fb
ademsoydan <ademm.soydan@gmail.com>
2023-05-27 22:14:40 +0300

    Revert "Init Projects And Add Layers"
    This reverts commit 37b654277fce882b7a0fed86c421022609f835ad.

--- src/main/java/com/wecan/footballapp/repository/TitleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wecan.footballapp.repository;
-
-import com.wecan.footballapp.entity.Title;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TitleRepository extends JpaRepository<Title,Integer> {
-}

22eec8c145eaede0c29ebf00068a542525ef391a
ademsoydan <ademm.soydan@gmail.com>
2023-06-03 02:09:48 +0300

    Add Send Entry Func

--- src/main/java/com/wecan/sportsforumapi/FootballappApplication.java
+++ src/main/java/com/wecan/sportsforumapi/FootballappApplication.java
@@ -26,22 +26,7 @@ public class FootballappApplication {
 	}
 
 	private void createInstrcutor(EntryService entryService, TitleService titleService, UserRepository userRepository) {
-		Title title = new Title(-1, "Diego Rossi", null);
-		title = titleService.saveTitle(title);
-		User adem = new User(-1,"adem","ademm.soydan@gmail.com","123", null);
-		User ali = new User(-1,"ali","ali.koc@gmail.com","123",null);
-		User jorge = new User(-1,"jorge","jorge.jesus@gmail.com","123",null);
-		adem = userRepository.save(adem);
-		ali = userRepository.save(ali);
-		jorge = userRepository.save(jorge);
 
-		Entry entry = new Entry(-1,"Düğünümde oynatmamHemen Satın Gitsin bu lavuğu",title,adem,0);
-		Entry entry2 = new Entry(-1,"Aslında fena değil ya",title,ali,0);
-		Entry entry3 = new Entry(-1,"Aşığım bu çocuğa",title,jorge,0);
-
-		entryService.save(entry);
-		entryService.save(entry2);
-		entryService.save(entry3);
 	}
 
 
